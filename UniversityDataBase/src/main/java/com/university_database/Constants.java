0933117a15c3bfac791385d6a9698270b90ed4fe
ahakimi <ahakimi@iastate.edu>
2025-03-22 14:54:11 -0500

    created a constants file

---
+++ UniversityDataBase/src/main/java/com/university_database/Constants.java
@@ -0,0 +1,8 @@
+package com.university_database;
+
+public class Constants {
+
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    public static final String USER = "";
+    public static final String PASS = "";
+}

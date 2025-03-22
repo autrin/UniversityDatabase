b53aae5d130f9798e7a40c6e3460c4abb8eefa15
ahakimi <ahakimi@iastate.edu>
2025-03-22 15:01:00 -0500

    added createDepartmentTable

--- UniversityDataBase/src/main/java/com/university_database/CreateTables.java
+++ UniversityDataBase/src/main/java/com/university_database/CreateTables.java
@@ -1,30 +1,41 @@
 package com.university_database;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import com.university_database.Constants;
 
 public class CreateTables {
 
-    public void createStudentTable(Statement stmt) throws SQLException{
-      String sql = "CREATE TABLE students("
-      + "sid INTEGER UNIQUE NOT NULL,"
-      + "ssn INTEGER PRIMARY KEY,"
-      + "name VARCHAR(20) NOT NULL,"
-      + "gender VARCHAR(1) NOT NULL,"
-      + "dob VARCHAR(10) NOT NULL,"
-      + "p_addr VARCHAR(20) NOT NULL,"
-      + "p_phone VARCHAR(20) NOT NULL,"
-      + "c_addr VARCHAR(20) NOT NULL,"
-      + "c_phone VARCHAR(20) NOT NULL)";
-      try {
-         stmt.executeUpdate(sql);
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
+    public void createStudentTable(Statement stmt) throws SQLException {
+        String sql = "CREATE TABLE students("
+                + "sid INTEGER UNIQUE NOT NULL,"
+                + "ssn INTEGER PRIMARY KEY,"
+                + "name VARCHAR(20) NOT NULL,"
+                + "gender VARCHAR(1) NOT NULL,"
+                + "dob VARCHAR(10) NOT NULL,"
+                + "p_addr VARCHAR(20) NOT NULL,"
+                + "p_phone VARCHAR(20) NOT NULL,"
+                + "c_addr VARCHAR(20) NOT NULL,"
+                + "c_phone VARCHAR(20) NOT NULL)";
+        try {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createDepartmentTable(Statement stmt) throws SQLException {
+        String sql = "CREATE TABLE departments ("
+                + "dcode INTEGER PRIMARY KEY,"
+                + "dname VARCHAR(50) UNIQUE NOT NULL,"
+                + "phone VARCHAR(10) NOT NULL,"
+                + "college VARCHAR(20) NOT NULL)";
+        try {
+            
+        } catch (SQLException e) {
+            
+        }
     }
 
     public static void main(String[] args) {
@@ -32,7 +43,7 @@ public class CreateTables {
         CreateTables tables = new CreateTables();
         try (
                 Connection conn = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS); Statement stmt = conn.createStatement();) {
-                  tables.createStudentTable(stmt);
+            tables.createStudentTable(stmt);
             System.out.println("Created student table in given database...");
         } catch (SQLException e) {
             e.printStackTrace();

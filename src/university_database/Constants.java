b4c1bbb2c0349d64fbfcc89bc695a96102dbabc3
Autrin <autrinhakimi@gmail.com>
2025-03-24 21:25:38 -0500

    allow loading local file

--- src/university_database/Constants.java
+++ src/university_database/Constants.java
@@ -2,7 +2,7 @@ package university_database;
 
 public class Constants {
 
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/university1";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/university1?allowLoadLocalInfile=true";
 
     public static final String USER = "coms363";
     public static final String PASS = "password";

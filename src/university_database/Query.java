146ca38d58913c0b098ae378dc60822ace133ac1
Autrin <autrinhakimi@gmail.com>
2025-03-25 13:37:05 -0500

    added query1() signature and created conn and stmt and query instances

--- src/university_database/Query.java
+++ src/university_database/Query.java
@@ -1,9 +1,24 @@
 package university_database;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class Query {
 
+	public void query1(Statement stmt) {
+		/*
+		 * The numbers and names of courses and their corresponding average grades from
+		 * students registered in the past semesters
+		 */
+		
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Starting Query process...");
+		Connection conn = null;
+		Statement stmt = null;
+		Query query = new Query();
+		
 
 	}
 
